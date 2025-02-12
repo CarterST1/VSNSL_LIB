935690585b5b6ed1c5f2ac06c06b23b8d954bd2f
CarterST1 <cobertrand@outlook.com>
2025-02-11 20:16:07 -0600

    Include author, version, and license in __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+"""
+VSNSL Package
+
+This package provides functionality for encoding and decoding data using the VSNSL class.
+
+Author: Your Name
+Version: 0.1.0
+License: MIT
+"""
+
+from .VSNSL_LIB import VSNSL  # Import the VSNSL class for use in the package
+
+__all__ = ['VSNSL']  # Specify what is exported when `from package import *` is used
+
+# Package metadata
+__author__ = "CarterST1"
+__version__ = (0,1,3)
+__license__ = "MIT"
